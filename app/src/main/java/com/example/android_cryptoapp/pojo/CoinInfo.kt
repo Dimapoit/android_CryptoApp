ce54f43c6d36c517b2bcba25cbeded536cba809c
Dimapoit <dimapoit88@gmail.com>
2023-03-06 23:29:42 +0200

    pojo

---
+++ app/src/main/java/com/example/android_cryptoapp/pojo/CoinInfo.kt
@@ -0,0 +1,22 @@
+package com.example.android_cryptoapp.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinInfo(
+    @SerializedName("Id")
+    @Expose
+    val id: String? = null,
+
+    @SerializedName("Name")
+    @Expose
+    val name: String? = null,
+
+    @SerializedName("FullName")
+    @Expose
+    val fullName: String? = null,
+
+    @SerializedName("ImageUrl")
+    @Expose
+    val ImageUrl: String? = null,
+)
