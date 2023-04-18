8ee02f0711d749994326c18b3ef84a6e9409a723
Dimapoit <dimapoit88@gmail.com>
2023-04-18 20:55:46 +0300

    up lifecycle version del Transformations

--- app/src/main/java/com/example/android_cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/android_cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.android_cryptoapp.data.repository
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
+//import androidx.lifecycle.Transformations
 import androidx.lifecycle.map
 import androidx.work.ExistingWorkPolicy
 import androidx.work.WorkManager
@@ -19,23 +19,23 @@ class CoinRepositoryImpl(private val application: Application) : CoinRepository 
 
 
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
-        return Transformations.map(coinInfoDao.getPriceList()) {
-            it.map { mapper.dbModelToEntity(it) }
-        }
-//        return coinInfoDao.getPriceList().map {
-//            it.map {
-//                mapper.dbModelToEntity(it)
-//            }
+//        return Transformations.map(coinInfoDao.getPriceList()) {
+//            it.map { mapper.dbModelToEntity(it) }
 //        }
+        return coinInfoDao.getPriceList().map {
+            it.map {
+                mapper.dbModelToEntity(it)
+            }
+        }
     }
 
     override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
-        return Transformations.map(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
-            mapper.dbModelToEntity(it)
-        }
-//        return coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map {
+//        return Transformations.map(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
 //            mapper.dbModelToEntity(it)
 //        }
+        return coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map {
+            mapper.dbModelToEntity(it)
+        }
     }
 
     override suspend fun loadData() {
